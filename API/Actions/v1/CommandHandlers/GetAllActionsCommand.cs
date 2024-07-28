231f5a3605ea3a59495d9aeb5d7be371a4886868
BhavyaMahajan21 <bhavyamahajan2197@gmail.com>
2024-07-28 18:57:37 +0530

    UserName Test

---
+++ API/Actions/v1/CommandHandlers/GetAllActionsCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace API.Actions.v1.CommandHandlers
+{
+    //Command
+    public class GetAllActionsCommand : IRequest<ICollection<Action>>
+    {
+        public ICollection<Action> Actions { get; set; }
+    }
+
+    //Command Handler
+    public class GetAllActionsCommandHandler : IRequestHandler<GetAllActionsCommand, ICollection<Action>>
+    {
+        public Task<ICollection<Action>> Handle(GetAllActionsCommand request, CancellationToken cancellationToken)
+        {
+            return new List<Action>();
+        }
+    }
+}
